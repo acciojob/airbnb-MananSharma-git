e4970190f8173fd3b694cc653f224179f1a18070
Manan <manansharma850@gmail.com>
2023-05-05 11:32:23 +0530

    test

--- src/main/java/com/driver/AirBnb.java
+++ src/main/java/com/driver/AirBnb.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AirBnb {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AirBnb.class, args);
+
+		System.out.print("");
 	}
 
 }
